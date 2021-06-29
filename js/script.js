a79d19280ae906e8988075dc3e2daf6c655c7bed
AlexTaglia <alex.tagliabue@gmail.com>
2021-06-29 14:46:51 +0200

    Initial commit, preparato vue js

---
+++ js/script.js
@@ -0,0 +1,38 @@
+Vue.config.devtools = true;
+
+/*
+Rifare l’esercizio della to do list come fatto assieme in classe:
+- stampare in pagina un item per ogni elemento contenuto in un array
+- ogni item ha una “x” associata: cliccando su di essa, l’item viene rimosso dalla lista
+- predisporre un input per aggiungere un nuovo item alla lista: digitando il tasto invio oppure ciccando su un pulsante, il testo digitato viene aggiunto alla lista
+*/
+
+new Vue(
+    {
+        el: '#app',
+
+        // -----------------------------
+        data: {
+            toDoList: [
+                'Fare l\'esercizio',
+            ],
+            newTask:'',
+        },
+
+        // -----------------------------
+        methods: {
+            addTask: function(){
+                if(this.newTask !== ''){
+                    this.toDoList.push(this.newTask)
+                    this.newTask = '';
+                }
+            },
+            removeTask: function(index){
+                this.toDoList.splice(index, 1)
+            }
+
+        }
+        
+    }
+
+)
